5c785d019b20f830a0db1d3e380a33e243dfdedb
Ujjwal Kanth <uknth@outlook.com>
2020-03-17 17:56:55 +0530

    feat: kafka consumer

--- base/transport/kafka/consumer.go
+++ base/transport/kafka/consumer.go
@@ -1,22 +1,59 @@
 package kafka
 
 import (
+	"context"
+	"time"
+
+	"github.com/go-kit/kit/transport"
+	"github.com/pkg/errors"
 	kafgo "github.com/segmentio/kafka-go"
+	"github.com/unbxd/go-base/base/endpoint"
 )
 
 type (
 	// ConsumerOption provies set of options to modify a subscriber
 	ConsumerOption func(*Consumer)
 
+	// Decoder decodes the message recieved on Kafka and converts in
+	// business logic
+	Decoder func(context.Context, kafgo.Message) (interface{}, error)
+
+	// BeforeFunc is executed prior to invoking the endpoint. RequestFunc
+	// may take information from request recieved in the Consumer
+	// and put it in the context.
+	// For instance, if the context needs the information about the topic
+	// or the group-id, that is populated here
+	BeforeFunc func(context.Context, kafgo.Message) context.Context
+
+	// AfterFunc are invoked after executing endpoint
+	AfterFunc func(context.Context, kafgo.Message, interface{}) context.Context
+
+	// ErrorFunc handles the error condition
+	ErrorFunc func(context.Context, kafgo.Message, error)
+
+	// ErrorHandler is wrapper on top of kit.transport.ErrorHandler
+	ErrorHandler interface{ transport.ErrorHandler }
+
 	// Consumer is kafka Consumer
 	Consumer struct {
+		autocommit bool
+
 		reader *kafgo.Reader
 		config *kafgo.ReaderConfig
+
+		end     endpoint.Endpoint
+		dec     Decoder
+		befores []BeforeFunc
+		afters  []AfterFunc
+		errFn   ErrorFunc
+
+		errHandler ErrorHandler
 	}
 )
 
 const (
 	defaultConsumerGroupID = "go-base-consumer"
+	defaultTopic           = "go-base-test"
 )
 
 // WithGroupIDConsumerOption provides an option to modify the GroupID for
@@ -44,10 +81,117 @@ func WithMaxMinByteConsumerOption(min, max int) ConsumerOption {
 	}
 }
 
+// WithAutoCommitConsumerOption sets the autocommit property of consumer
+func WithAutoCommitConsumerOption(flag bool) ConsumerOption {
+	return func(c *Consumer) { c.autocommit = flag }
+}
+
+// WithAutoCommitTimeConsumerOption sets the auto commit time for Consumer
+func WithAutoCommitTimeConsumerOption(dur time.Duration) ConsumerOption {
+	return func(c *Consumer) { c.config.CommitInterval = dur }
+}
+
+// WithDecoderConsumerOption sets the decoder for the Consumer Message
+func WithDecoderConsumerOption(fn Decoder) ConsumerOption {
+	return func(c *Consumer) { c.dec = fn }
+}
+
+// WithErrorFuncConsumerOption provides a callback to handle error
+func WithErrorFuncConsumerOption(fn ErrorFunc) ConsumerOption {
+	return func(c *Consumer) { c.errFn = fn }
+}
+
+// WithBeforeFuncsConsumerOption provides a way to set BeforeFunc(s)
+// to the consumer
+func WithBeforeFuncsConsumerOption(fns ...BeforeFunc) ConsumerOption {
+	return func(c *Consumer) { c.befores = append(c.befores, fns...) }
+}
+
+// WithAfterFuncsConsumerOption provides a way to set AfterFunc(s)
+// to the consumer
+func WithAfterFuncsConsumerOption(fns ...AfterFunc) ConsumerOption {
+	return func(c *Consumer) { c.afters = append(c.afters, fns...) }
+}
+
+// Open actually handles the subcriber messages
+func (c *Consumer) Open() error {
+	for {
+		// start a new context
+		var (
+			ctx = context.Background()
+			msg kafgo.Message
+			err error
+		)
+
+		if c.autocommit {
+			msg, err = c.reader.ReadMessage(ctx)
+		} else {
+			msg, err = c.reader.FetchMessage(ctx)
+		}
+
+		if err != nil {
+			c.errFn(ctx, msg, errors.Wrap(
+				err, "read message from kafka failed",
+			))
+			c.errHandler.Handle(ctx, err)
+			continue
+		}
+
+		// before endpoint
+		for _, fn := range c.befores {
+			ctx = fn(ctx, msg)
+		}
+
+		rq, err := c.dec(ctx, msg)
+		if err != nil {
+			c.errFn(ctx, msg, err)
+			c.errHandler.Handle(ctx, err)
+			continue
+		}
+
+		// execute endpoint
+		rs, err := c.end(ctx, rq)
+		if err != nil {
+			c.errFn(ctx, msg, err)
+			c.errHandler.Handle(ctx, err)
+			continue
+		}
+
+		for _, fn := range c.afters {
+			ctx = fn(ctx, msg, rs)
+		}
+
+		if !c.autocommit {
+			err = c.reader.CommitMessages(ctx, msg)
+			if err != nil {
+				c.errFn(ctx, msg, err)
+				c.errHandler.Handle(ctx, err)
+				continue
+			}
+		}
+	}
+}
+
 // NewKafkaConsumer returns kafka consumer for the given brokers
 func NewKafkaConsumer(
 	brokers []string,
 	options ...ConsumerOption,
 ) (*Consumer, error) {
+	// default values
+	cfg := kafgo.ReaderConfig{
+		Brokers: brokers,
+		GroupID: defaultConsumerGroupID,
+		Topic:   defaultTopic,
+	}
 
+	cs := &Consumer{
+		reader: kafgo.NewReader(cfg),
+		config: &cfg,
+	}
+
+	for _, o := range options {
+		o(cs)
+	}
+
+	return cs, nil
 }
--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/VividCortex/gohistogram v1.0.0 // indirect
 	github.com/dimfeld/httptreemux/v5 v5.0.2
 	github.com/go-kit/kit v0.9.0
-	github.com/nats-io/nats.go v1.8.1
 	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c
 	github.com/pkg/errors v0.8.1
 	github.com/prometheus/client_golang v1.1.0
