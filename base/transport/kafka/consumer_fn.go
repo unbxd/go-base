fc2ac72abf67f72e686219f0f88197619c3b4e81
Ujjwal Kanth <uknth@outlook.com>
2020-03-20 16:27:07 +0530

    feat: utility functions for kafka consumer

---
+++ base/transport/kafka/consumer_fn.go
@@ -0,0 +1,22 @@
+package kafka
+
+import (
+	"context"
+	"fmt"
+
+	kafgo "github.com/segmentio/kafka-go"
+)
+
+func defaultErrorFunc(
+	ctx context.Context,
+	msg kafgo.Message,
+	err error,
+) {
+	fmt.Printf(
+		"Reader Err: [Topic: %v] [Partition: %v] [Key: %v] [Error: %v]",
+		msg.Topic,
+		msg.Partition,
+		msg.Key,
+		err.Error(),
+	)
+}
