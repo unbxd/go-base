d45da005ebb5e65f3d9727833d0c1cafbeef2546
Apoorv Bhawsar <apoorvqwerty@gmail.com>
2021-09-26 23:12:10 +0530

    feat: add grpc transport

---
+++ kit/transport/grpc/transport.go
@@ -0,0 +1,39 @@
+package grpc
+
+import (
+	"fmt"
+	"net"
+
+	grpc "google.golang.org/grpc"
+)
+
+type (
+	// TransportOption sets options parameters to Transport
+	TransportOption func(*Transport)
+
+	// Transport is a wrapper around grpc.Server
+	Transport struct {
+		*grpc.Server
+		Port int
+	}
+)
+
+// Open starts the Transport
+func (tr *Transport) Open() error {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", tr.Port))
+	if err != nil {
+		return err
+	}
+	return tr.Serve(listener)
+}
+
+//NewTransport returns a new transport
+func NewTransport(options ...TransportOption) (*Transport, error) {
+	transport := &Transport{
+		Server: grpc.NewServer(),
+	}
+	for _, o := range options {
+		o(transport)
+	}
+	return transport, nil
+}
